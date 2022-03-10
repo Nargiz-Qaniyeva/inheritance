95df5bc98c0df8b4e2ee64669c0102668e58d4de
Nargiz-Qaniyeva <tu7lyw21z@code.edu.az>
2022-03-11 03:36:05 +0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Models/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    internal class Book: Product
+    {
+        //1.fields
+        public string name;
+        public int costPrice = 600; //default deyeri bu hissede veririk (15ci setir )
+        public int saledPrice;
+        public string authorName;
+        public int pageCount;
+        public int discountPercent;
+
+        
+        
+        //constructor
+        //1.ilk ise dusen methotdur
+        //2.return type olmur
+        //classin adi ile eynidi
+
+
+        public void fullName()
+        {
+            Console.WriteLine("name");
+        }
+
+
+        public int common()     //class-in daxilindeki method
+        {
+            Console.WriteLine($" {costPrice } {saledPrice }");
+            return costPrice;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿ using System;
+ using ConsoleApp1.Models;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            //2.instance
+            //2.(intro)-yaratdigimiz obyektlere instance deyirik. yeni product type-den bir product cixaracaq
+            //type-i product oldugu ucun onunla yaziriq
+            Product computer = new Product();
+            //computerin bizde name, costprice, saledprice-i oldugu ucun, onlari yazdiririq
+            computer.name = "Acer";     // stringi set elemesem null - bosluq verecek
+            computer.costPrice = 1500; //deyerleri elave elemesek avt. 0 verecek.cunki classda int default deyeri 0-di
+            computer.saledPrice = 2000;
+            Console.WriteLine(computer .costPrice  );
+            computer.common();    //39cu setiri oxutdu  
+
+
+            Book publication = new Book();
+            publication.name = "Robin Hood";
+            publication.costPrice = 5;
+            publication.saledPrice = 9;
+            publication.authorName = "J.Walker";
+            publication.pageCount = 217;
+            publication.discountPercent = 5;
+            Console.WriteLine(publication .authorName);
+
+
+
+        }
+       
+
+    }
+
+    class Product   //product artiq bir tipdir. classin type-i
+    {
+        internal string name;
+        internal int costPrice;
+        internal int saledPrice;
+
+        internal void common()
+        {
+           // throw new NotImplementedException();
+        }
+    }
+
+}
